a1120a3b6dde8308e89e55f2cc24cc04daab399a
noceive <nocivescience@gmail.com>
2022-07-22 02:39:38 -0400

    terminado

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 (function() {
     const wheel = document.querySelector('.wheel');
-    const startButton = document.querySelector('.button');
+    const startButton = document.querySelector('.btn');
+    const indexEl=document.querySelector('.index')
     let deg = 0;
-  
     startButton.addEventListener('click', () => {
-      // Disable button during spin
       startButton.style.pointerEvents = 'none';
-      // Calculate a new rotation between 5000 and 10 000
       deg = Math.floor(5000 + Math.random() * 5000);
-      // Set the transition on the wheel
       wheel.style.transition = 'all 10s ease-out';
-      // Rotate the wheel
+      indexEl.style.transition='all 10s linear';
       wheel.style.transform = `rotate(${deg}deg)`;
-      // Apply the blur
+      indexEl.style.transform='rotate(360deg)';
       wheel.classList.add('blur');
     });
-  
     wheel.addEventListener('transitionend', () => {
-      // Remove blur
       wheel.classList.remove('blur');
-      // Enable button when spin is over
       startButton.style.pointerEvents = 'auto';
-      // Need to set transition to none as we want to rotate instantly
       wheel.style.transition = 'none';
-      // Calculate degree on a 360 degree basis to get the "natural" real rotation
-      // Important because we want to start the next spin from that one
-      // Use modulus to get the rest value from 360
       const actualDeg = deg % 360;
-      // Set the real rotation instantly without animation
+      console.log(actualDeg)
       wheel.style.transform = `rotate(${actualDeg}deg)`;
     });
+    indexEl.addEventListener('transitionend',()=>{
+        startButton.style.pointerEvents='auto';
+        indexEl.style.transition='none';
+        indexEl.style.transform='rotate(0deg)';
+    })
   })();
   
